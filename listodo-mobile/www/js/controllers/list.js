01944a3726fc985d573c397f0e3791aeb640b197
Cédric <cedced19@gmail.com>
2016-03-16 11:29:47 +0100

    Get lists from Local Storage before Server

--- listodo-mobile/www/js/controllers/list.js
+++ listodo-mobile/www/js/controllers/list.js
@@ -7,14 +7,11 @@ app.controller('ListodoTasksCtrl', function ($scope, $rootScope, $location, loca
         $location.path('/tasks/' + encodeURI(list.name) + '/' + encodeURI(task.name));
     };
 
-    if ($cordovaNetwork.isOffline()) {
-        $scope.lists = localStorageService.get('lists').concat(localStorageService.get('listsToPublish'));
-    } else {
+    $scope.lists = localStorageService.get('lists').concat(localStorageService.get('listsToPublish'));
+    if ($cordovaNetwork.isOnline()) {
         $http.get('http://' + localStorageService.get('adress') + '/api/lists').success(function (data) {
             $scope.lists = localStorageService.get('listsToPublish').concat(data);
             localStorageService.get('lists') = data;
-        }).error(function () {
-            $scope.lists = localStorageService.get('lists').concat(localStorageService.get('listsToPublish'));
         });
     }
 });
